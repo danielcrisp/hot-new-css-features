73516d287c041f9383e321db97f1bb4e6412c9cc
danielcrisp <daniel@danielcrisp.com>
2018-03-21 18:01:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Hot new CSS features
+
+This repo accompanies a tutorial written for Web Designer magazine which shows
+you how and when to use five hot new CSS features.
+
+ - `display: contents;`
+ - `@supports(...) {...}` feature queries
+ - `overscroll-behavior: contain;`
+ - `:focus-within` and `:placeholder-shown` pseudo-classes
+ - `contain: paint;`
+
+## Requirements
+
+ - Node.js
+ - npm
+ - A modern browser
+
+## Install
+
+    npm install
+
+## Start
+
+    npm start
+
+Your browser will automatically open the tutorial.
+
+---
+
+Made by Daniel Crisp - [danielcrisp.com](https://danielcrisp.com)
---
+++ src/step-18/script.js
@@ -0,0 +1,40 @@
+// Finds the important elements
+const input = document.getElementById('input');
+const messages = document.getElementById('messages');
+
+// Listens for keypress events on the input
+input.addEventListener('keypress', (event) => {
+    // Checks if the key pressed was ENTER
+    if (event.keyCode === 13) {
+        // Checks the field is valid
+        if (input.validity.valid) {
+            // Creates a DOM element using the value
+            const message = createMessage(input.value);
+            // Appends the new message to the list
+            messages.appendChild(message);
+            // Clears the field
+            input.value = '';
+            // Scrolls the messages to the bottom
+            messages.parentNode.scrollTop = messages.parentNode.scrollHeight;
+        }
+    }
+});
+
+// Converts value to string of HTML
+function createMessage (value) {
+    // Warning: Don't do this in production without sanitizing the string first!
+    return stringToDom(`
+      <li>
+        <div class="message message--mine" data-timestamp="${new Date().toString()}">
+          ${value}
+        </div>
+      </li>
+    `);
+}
+
+// Converts string to real DOM
+function stringToDom (string) {
+    const template = document.createElement('template');
+    template.innerHTML = string.trim();
+    return template.content.firstChild;
+}
